453a12ae55050db7c18cb66846b4e091e7b6cbf5
Rafe01 <130284639+rafe000000101010101010@users.noreply.github.com>
2023-11-04 09:27:44 -0700

    Add files via upload

--- return.cs
+++ return.cs
@@ -5,16 +5,18 @@ class Program
 {
     static void Main()
     {
+        /*    {
         int num01;
         int num02;
 
         Console.Write("num01: ");
         num01 = int.Parse(Console.ReadLine());
-        Console.Write("num01: ");
-        num02 = int.Parse(Console.ReadLine());
-
+        Console.Write("num02: ");
+        num02 = int.Parse(Console.ReadLine());          //firts to understand
+        
         int result = Addition(num01, num02);
-        Console.WriteLine("Ans: " + result);
+        Console.WriteLine("Answer: "result);
+        Console.ReadKey();
     }
 
     static int Addition(int num01, int num02)
@@ -22,5 +24,66 @@ class Program
         int result = num01 + num02;
 
         return result;
+    } */
+        /*   int num01;
+           int num02;
+   
+           Console.Write("num01: ");
+           num01 = int.Parse(Console.ReadLine());              //second to understand
+           Console.Write("num02: ");
+           num02 = int.Parse(Console.ReadLine());
+   
+           string result = Addition(num01, num02);
+           Console.WriteLine(result);
+           Console.ReadKey();
+       }
+   
+       static string Addition(int num01, int num02)
+       {
+           int result = num01 + num02;
+   
+           return $"Answer is: {result}";
+       }
+   }*/
+
+        /* int num01;
+         int num02;
+ 
+         Console.Write("num01: ");
+         num01 = int.Parse(Console.ReadLine());
+         Console.Write("num02: ");
+         num02 = int.Parse(Console.ReadLine());
+ 
+         int result = Addition(num01, num02);
+         Console.WriteLine(result);
+         Console.ReadKey();
+     }
+ 
+     static int Addition(int num01, int num02)
+     {
+         if (num01 > num02)
+         {
+             int result = num01 + num02;
+             return result;
+         }
+         else
+         {
+             int result = num01 - num02;
+             return result;
+         }
+     }
+ }*/
+        Addition(5, 3);
+        Console.ReadKey();
+    }
+
+    static void Addition(int num01, int num02)
+    {
+        if (num01 > num02)
+        {
+            return;
+        }
+
+        Console.WriteLine("You are succesfull!");
     }
 }
